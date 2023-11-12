fd654c5466f053dfab82a2b8818d2bc0bf05523d
Aleksandra Szcześniak <136647548+aleksandra-szczesniak@users.noreply.github.com>
2023-11-12 16:43:19 +0100

    setup.py

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -4,7 +4,9 @@ setup(
     name="clean-folder",
     version="1.0",
     packages=["clean_folder"],
-    install_requires=[],
+    install_requires=[
+        'unidecode'
+    ],
     entry_points={
         "console_scripts": [
             "clean-folder = clean_folder.clean:main"
